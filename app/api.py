5b149ca58f2f857679e379b79860f45ab4dd3d7c
Ramin Mardani <ramin_mardani@hotmail.com>
2024-03-28 12:20:27 -0700

    Refactored

--- app/api.py
+++ app/api.py
@@ -1,6 +1,5 @@
+# Import necessary libraries
 import warnings
-
-warnings.filterwarnings("ignore")
 from service import main
 from fastapi import FastAPI, HTTPException, File, UploadFile, Query
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,17 +8,25 @@ import datetime
 from pydantic import BaseModel
 from typing import Optional
 
-################################################## FASTAPI ########################################
+# Ignore warnings
+warnings.filterwarnings("ignore")
+
+# Define a FastAPI application
 app = FastAPI(docs_url="/llm-rag/docs", openapi_url="/llm-rag/openapi.json")
 
 
+# Define a Pydantic model for request body
 class Item(BaseModel):
     q: str = "Ask a question from your document..."
 
 
+# Define a HTTP Basic security
 security = HTTPBasic()
+
+# Define allowed origins for CORS
 origins = ["*"]
 
+# Add CORS middleware to the application
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -29,19 +36,22 @@ app.add_middleware(
 )
 
 
+# Define an endpoint to list documents available in DB
 @app.post("/llm-rag/list_docs")
 async def List_documents_available_in_DB():
     return main.list_docs_pg()
 
 
+# Define an endpoint to upload new files to DB
 @app.post("/llm-rag/upload_docs")
 async def Upload_new_files_to_DB(upload_file: UploadFile = File(...)):
-    if upload_file.content_type not in [
+    allowed_content_types = [
         "application/pdf",
         "application/msword",
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         "text/plain",
-    ]:
+    ]
+    if upload_file.content_type not in allowed_content_types:
         raise HTTPException(
             400,
             detail="Invalid document type. Only PDF, Doc, Docx, and txt files are allowed.",
@@ -50,6 +60,7 @@ async def Upload_new_files_to_DB(upload_file: UploadFile = File(...)):
         return main.upload_file_pg(upload_file)
 
 
+# Define an endpoint to delete existing files from DB
 @app.post("/llm-rag/delete_doc")
 async def Delete_existing_files_from_DB(file_name: Optional[str] = "document1.pdf"):
     if file_name is None:
@@ -57,6 +68,7 @@ async def Delete_existing_files_from_DB(file_name: Optional[str] = "document1.pd
     return main.delete_doc_pg(file_name)
 
 
+# Define an endpoint to ask questions from your documents
 @app.post("/llm-rag/question")
 async def QA_from_your_documents(
     query: Item, llm_model: str = Query("OpenAI", enum=["OpenAI", "LLM"])
@@ -64,6 +76,7 @@ async def QA_from_your_documents(
     return main.question_pg(query.q, llm_model)
 
 
+# Define an endpoint to check service status
 @app.get("/llm-rag/service_status")
 async def service_status():
-    return {"Status: OK, Date:{}".format(datetime.datetime.now())}
+    return {"Status": "OK", "Date": datetime.datetime.now().isoformat()}
--- app/service/main.py
+++ app/service/main.py
@@ -2,65 +2,31 @@ import warnings
 import os
 import psycopg2
 import nltk
-
-nltk.download("punkt")
-from huggingface_hub._login import _login
+from huggingface_hub import login as hf_login
 from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores.pgvector import DistanceStrategy
-from langchain_community.document_loaders import DirectoryLoader
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
+from langchain_community.document_loaders.merge import MergedDataLoader
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain_community.llms import HuggingFacePipeline
 from langchain_community.vectorstores.pgvector import PGVector
-from langchain_community.document_loaders.merge import MergedDataLoader
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
 
-_login(token=os.environ["HUGGINGFACE_TOKEN"], add_to_git_credential=False)
+# Suppress specific warnings and download necessary NLTK data
+nltk.download("punkt")
+hf_login(token=os.environ["HUGGINGFACE_TOKEN"], add_to_git_credential=False)
 warnings.filterwarnings("ignore", category=UserWarning)
 
-
 def write_file(file_path, file_contents):
+    """Writes binary content to a file."""
     with open(file_path, "wb") as file:
         file.write(file_contents)
 
-
-def list_docs_pg() -> list:
-    try:
-        # Connect to the database
-        conn = psycopg2.connect(
-            user=os.environ["PGVECTOR_USER"],
-            password=os.environ["PGVECTOR_PASSWORD"],
-            host=os.environ["PGVECTOR_HOST"],
-            port=os.environ["PGVECTOR_PORT"],
-            database=os.environ["PGVECTOR_DB"],
-        )
-
-        # Create a cursor to execute SQL commands
-        cur = conn.cursor()
-
-        # Now you can proceed with your SELECT query
-        cur.execute("SELECT file_name FROM files_table")
-
-        # Fetch all the rows
-        files = cur.fetchall()
-
-        # Close cursor and connection
-        cur.close()
-        conn.close()
-
-        # Return the list of filenames
-        return [row[0] for row in files]
-    except psycopg2.Error as e:
-        return "Error retrieving documents from the database:" + str(e)
-
-
-def upload_file_pg(file):
-    filename = file.filename
-    file_contents = file.file.read()
+def db_connect():
+    """Creates and returns a database connection and cursor."""
     conn = psycopg2.connect(
         user=os.environ["PGVECTOR_USER"],
         password=os.environ["PGVECTOR_PASSWORD"],
@@ -68,195 +34,113 @@ def upload_file_pg(file):
         port=os.environ["PGVECTOR_PORT"],
         database=os.environ["PGVECTOR_DB"],
     )
-
-    # Create a cursor to execute SQL commands
     cur = conn.cursor()
+    return conn, cur
 
+def list_docs_pg() -> list:
+    """Lists document filenames from the database."""
     try:
-        # Check if the file already exists in the database
-        cur.execute(
-            "SELECT COUNT(*) FROM files_table WHERE file_name = %s", (filename,)
-        )
-        result = cur.fetchone()[0]
+        conn, cur = db_connect()
+        cur.execute("SELECT file_name FROM files_table")
+        files = cur.fetchall()
+        return [row[0] for row in files]
+    except psycopg2.Error as e:
+        return f"Error retrieving documents from the database: {e}"
+    finally:
+        cur.close()
+        conn.close()
 
-        if result == 0:
-            # If the file doesn't exist, insert it into the database
-            cur.execute(
-                "INSERT INTO files_table (file_name, file_content) VALUES (%s, %s)",
-                (filename, file_contents),
-            )
+def upload_file_pg(file):
+    """Uploads a file to the database if it doesn't already exist."""
+    filename = file.filename
+    file_contents = file.file.read()
+    try:
+        conn, cur = db_connect()
+        cur.execute("SELECT COUNT(*) FROM files_table WHERE file_name = %s", (filename,))
+        if cur.fetchone()[0] == 0:
+            cur.execute("INSERT INTO files_table (file_name, file_content) VALUES (%s, %s)", (filename, file_contents))
             conn.commit()
             return "File uploaded and inserted into the database."
         else:
             return "File with the same name already exists."
     except psycopg2.Error as e:
-        # Rollback the transaction in case of an error
         conn.rollback()
-        return "Error inserting document into the database: {}".format(e)
+        return f"Error inserting document into the database: {e}"
     finally:
-        # Close the cursor and the connection
         cur.close()
         conn.close()
-
 
 def delete_doc_pg(file_name) -> str:
+    """Deletes a document from the database by filename."""
     try:
-        conn = psycopg2.connect(
-            user=os.environ["PGVECTOR_USER"],
-            password=os.environ["PGVECTOR_PASSWORD"],
-            host=os.environ["PGVECTOR_HOST"],
-            port=os.environ["PGVECTOR_PORT"],
-            database=os.environ["PGVECTOR_DB"],
-        )
-        with conn.cursor() as cur:
-            # Check if the file exists
-            cur.execute("SELECT 1 FROM files_table WHERE file_name = %s", (file_name,))
-            if cur.fetchone():
-                # File exists, delete it
-                cur.execute(
-                    "DELETE FROM files_table WHERE file_name = %s", (file_name,)
-                )
-                conn.commit()
-                return "Document deleted successfully."
-            else:
-                return "File does not exist."
+        conn, cur = db_connect()
+        cur.execute("SELECT 1 FROM files_table WHERE file_name = %s", (file_name,))
+        if cur.fetchone():
+            cur.execute("DELETE FROM files_table WHERE file_name = %s", (file_name,))
+            conn.commit()
+            return "Document deleted successfully."
+        else:
+            return "File does not exist."
     except psycopg2.Error as e:
         return f"Error deleting document: {e}"
-
-
-def question_pg(query: str, llm) -> str:
-    if llm == "OpenAI":
-
-        local_llm = ChatOpenAI(
-            model_name="gpt-4-0125-preview",
-            temperature=0,
-            top_p=0.95,
-        )
-
-        instructor_embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-
-    else:
-        model_name = "google/flan-t5-base"
-        tokenizer = AutoTokenizer.from_pretrained(
-            model_name, return_full_text=True, model_max_length=512, truncation=True
-        )
-        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-        pipe = pipeline(
-            "text2text-generation",  # Specify the task as text-to-text generation
-            model=model,  # Use the previously initialized model
-            tokenizer=tokenizer,  # Use the previously initialized tokenizer
-            max_length=512,  # Set the maximum length for generated text to 512 tokens
-            temperature=0,  # Set the temperature parameter for controlling randomness (0 means deterministic)
-            top_p=0.95,  # Set the top_p parameter for controlling the nucleus sampling (higher values make output more focused)
-            repetition_penalty=1.15,  # Set the repetition_penalty to control the likelihood of repeated words or phrases
-            truncation=True,  # Truncate the input to the model to the maximum length of the model
-            padding="max_length",  # Pad the input to the model to the maximum length of the model
-        )
-        local_llm = HuggingFacePipeline(pipeline=pipe)
-        embedding_model_name = "hkunlp/instructor-base"
-        instructor_embeddings = HuggingFaceInstructEmbeddings(
-            model_name=embedding_model_name,
-            model_kwargs={"device": "cpu"},
-        )
-    try:
-        conn = psycopg2.connect(
-            user=os.environ["PGVECTOR_USER"],
-            password=os.environ["PGVECTOR_PASSWORD"],
-            host=os.environ["PGVECTOR_HOST"],
-            port=os.environ["PGVECTOR_PORT"],
-            database=os.environ["PGVECTOR_DB"],
-        )
-        cur = conn.cursor()
-        cur.execute("SELECT file_name, file_content FROM files_table")
-        documents = cur.fetchall()
-
-        # Close the cursor and the connection
+    finally:
         cur.close()
         conn.close()
-        # Folder to save the extracted documents
+
+def question_pg(query: str, llm) -> str:
+    """Answers a question based on documents in the database using a specified language model."""
+    # Setup for OpenAI or HuggingFace models
+    if llm == "OpenAI":
+        local_llm = ChatOpenAI(model_name="gpt-4-0125-preview", temperature=0, top_p=0.95)
+        instructor_embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    else:
+        model_name = "google/flan-t5-base"
+        tokenizer = AutoTokenizer.from_pretrained(model_name, return_full_text=True, model_max_length=512, truncation=True)
+        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+        pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer, max_length=512, temperature=0, top_p=0.95, repetition_penalty=1.15, truncation=True, padding="max_length")
+        local_llm = HuggingFacePipeline(pipeline=pipe)
+        instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device": "cpu"})
+
+    try:
+        # Process documents for querying
+        conn, cur = db_connect()
+        cur.execute("SELECT file_name, file_content FROM files_table")
+        documents = cur.fetchall()
         output_folder = "./extracted_documents/"
-        if os.path.exists(output_folder):
-            files = os.listdir(output_folder)
-            for file_name in files:
-                file_path = os.path.join(output_folder, file_name)
-                os.remove(file_path)
-        else:
+        if not os.path.exists(output_folder):
             os.makedirs(output_folder)
-        # Iterate through each document and save it
         for document in documents:
             filename, file_contents = document
-            file_path = os.path.join(output_folder, filename)
-            write_file(file_path, file_contents)
+            write_file(os.path.join(output_folder, filename), file_contents)
 
-        files = os.listdir(output_folder)
-        # Check if the output folder exists, if not, create it
-        loader_pdf = DirectoryLoader(
-            output_folder,
-            glob="./*.pdf",
-            use_multithreading=True,
-            silent_errors=True,
-            loader_cls=PyPDFLoader,
-        )
-        loader_doc = DirectoryLoader(output_folder, glob="./*.doc")
-        loader_docx = DirectoryLoader(output_folder, glob="./*.docx")
-        loader_txt = DirectoryLoader(
-            output_folder,
-            glob="./*.txt",
-            loader_cls=TextLoader,
-            use_multithreading=True,
-            silent_errors=True,
-        )
-        loader = MergedDataLoader(
-            loaders=[loader_pdf, loader_doc, loader_docx, loader_txt]
-        )
+        # Load documents using various loaders
+        loaders = [
+            DirectoryLoader(output_folder, glob="./*.pdf", use_multithreading=True, silent_errors=True, loader_cls=PyPDFLoader),
+            DirectoryLoader(output_folder, glob="./*.doc"),
+            DirectoryLoader(output_folder, glob="./*.docx"),
+            DirectoryLoader(output_folder, glob="./*.txt", loader_cls=TextLoader, use_multithreading=True, silent_errors=True)
+        ]
+        loader = MergedDataLoader(loaders=loaders)
         documents = loader.load()
-        if len(documents) == 0:
+        if not documents:
             return "No documents were found in the database. Please upload a document first and try again."
-        text_splitter = CharacterTextSplitter(
-            separator="\n\n",
-            chunk_size=2000,
-            chunk_overlap=400,
-            length_function=len,
-            is_separator_regex=False,
-        )
 
+        # Split and embed documents for retrieval
+        text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=2000, chunk_overlap=400, length_function=len, is_separator_regex=False)
         documents = text_splitter.split_documents(documents)
-
-        connection_string = PGVector.connection_string_from_db_params(
-            driver=os.environ.get("PGVECTOR_DRIVER", "psycopg2"),
-            host=os.environ["PGVECTOR_HOST"],
-            port=int(os.environ["PGVECTOR_PORT"]),
-            database=os.environ["PGVECTOR_DB"],
-            user=os.environ["PGVECTOR_USER"],
-            password=os.environ["PGVECTOR_PASSWORD"],
-        )
-        db = PGVector.from_documents(
-            embedding=instructor_embeddings,
-            documents=documents,
-            collection_name="files_table",
-            distance_strategy=DistanceStrategy.COSINE,
-            connection_string=connection_string,
-            pre_delete_collection=True,
-        )
+        connection_string = PGVector.connection_string_from_db_params(driver=os.environ.get("PGVECTOR_DRIVER", "psycopg2"), host=os.environ["PGVECTOR_HOST"], port=int(os.environ["PGVECTOR_PORT"]), database=os.environ["PGVECTOR_DB"], user=os.environ["PGVECTOR_USER"], password=os.environ["PGVECTOR_PASSWORD"])
+        db = PGVector.from_documents(embedding=instructor_embeddings, documents=documents, collection_name="files_table", distance_strategy=DistanceStrategy.COSINE, connection_string=connection_string, pre_delete_collection=True)
         retriever = db.as_retriever(search_kwargs={"k": 3})
+
+        # Setup and execute the question-answering chain
         instruction = """CONTEXT:/n/n If the information is not available in the retriever, reply with "I am sorry, this information is not in my knowledge base.". Disregard information of the table of content of the retriever. Here are retriever information: {context}/n Question: {question}"""
-        llama_prompt = PromptTemplate(
-            template=instruction, input_variables=["context", "question"]
-        )
-        chain_type_kwargs = {"prompt": llama_prompt}
-        qa_chain = RetrievalQA.from_chain_type(
-            llm=local_llm,
-            chain_type="stuff",
-            retriever=retriever,
-            chain_type_kwargs=chain_type_kwargs,
-            return_source_documents=True,
-            verbose=True,
-        )
+        llama_prompt = PromptTemplate(template=instruction, input_variables=["context", "question"])
+        qa_chain = RetrievalQA.from_chain_type(llm=local_llm, chain_type="stuff", retriever=retriever, chain_type_kwargs={"prompt": llama_prompt}, return_source_documents=True, verbose=True)
         out = qa_chain(query)
         out["result"] = out["result"].replace(r"\"", " ")
-        if out["result"] == "I am sorry, this information is not in my knowledge base.":
-            return {"result": out["result"]}
-        else:
-            return out
+        return out if out["result"] != "I am sorry, this information is not in my knowledge base." else {"result": out["result"]}
     except psycopg2.Error as e:
         return f"Error retrieving documents from the database: {e}"
+    finally:
+        cur.close()
+        conn.close()
